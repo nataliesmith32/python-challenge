64ffce0910ce3181d5466bde14884c5650639af6
nataliesmith32 <natalielsmith32@gmail.com>
2020-12-16 00:36:48 -0700

    updates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,35 +18,42 @@ with open(election_csv) as csvfile:
     header = next(csvreader)
 
 #loop to calulcate vote totals
-for candidate in csvreader:
-    totalvotes += 1
-    if candidate[2] == "Khan":
-        candidate_1 += 1
-    elif candidate[2] == "Correy":
-        candidate_2 += 1
-    elif candidate[2] == "Li":
-        candidate_3 += 1
-    elif candidate[2] == "O'Tooley":
-        candidate_4 += 1
+    for candidate in csvreader:
+        totalvotes += 1
+        if candidate[2] == "Khan":
+            candidate_1 += 1
+        elif candidate[2] == "Correy":
+            candidate_2 += 1
+        elif candidate[2] == "Li":
+            candidate_3 += 1
+        elif candidate[2] == "O'Tooley":
+            candidate_4 += 1
 
 cnd1_pct = (candidate_1 / totalvotes) * 100
 cnd2_pct = (candidate_2 / totalvotes) * 100       
 cnd3_pct = (candidate_3 / totalvotes) * 100
 cnd4_pct = (candidate_4 / totalvotes) * 100
 
-all_runners = candidate_1 + candidate_2 + candidate_3 + candidate_4
-
-winner = max(all_runners)
+winner = max(candidate_1, candidate_2, candidate_3, candidate_4)
+if winner == candidate_1:
+    print("Khan")
+elif winner == candidate_2:
+    print("Correy")
+elif winner == candidate_3:
+    print("Li")
+elif winner == candidate_4:
+    election_winner = "O'Tooley"
 
 # print statements
 print(f"Election Results")
 print(f"---------------------------")
 print(f"Total Votes: {totalvotes}")
 print(f"---------------------------")
-print(f"Kahn: {cnd1_pct}({candidate_1})")
-print(f"Correy: {cnd2_pct}({candidate_2})")
-print(f"Li: {cnd3_pct}({candidate_3})")
-print(f"O'Tooley: {cnd4_pct}({candidate_4})")
+print(f"Kahn: {cnd1_pct:.3%}({candidate_1})")
+print(f"Correy: {cnd2_pct:.3%}({candidate_2})")
+print(f"Li: {cnd3_pct:.3%}({candidate_3})")
+print(f"O'Tooley: {cnd4_pct:.3%}({candidate_4})")
 print(f"---------------------------")
 print(f"Winner: {winner}")
 print(f"---------------------------")  
+
